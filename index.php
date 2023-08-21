569ceed9e36d6a94f509505fa85c4d8f19a47e6d
Rato <dudunaboa@outlook.com>
2023-08-21 19:56:05 -0300

    [new] index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Barra de Pesquisa</title>
+</head>
+<body>
+    <div class="search-container">
+        <input type="text" id="search-bar" placeholder="Digite sua pesquisa...">
+        <button id="search-button">Pesquisar</button>
+    </div>
+</body>
+</html>
